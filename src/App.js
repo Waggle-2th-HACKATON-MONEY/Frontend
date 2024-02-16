18b8170d09316dc7336fa07584d5111147734918
김낙도 <tpcmsk0920@gmail.com>
2024-02-17 02:44:58 +0900

    '질문페이지-구현'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
 import MainPage from "./components/MainPage";
+import QuestionPage from "./components/QuestionPage";
+
 function App() {
   return (
-    <MainPage />
+    <QuestionPage />
+    
   );
 }
 
