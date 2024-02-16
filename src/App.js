06111edeca724b97e5cbb7ad34f3054c7c72af58
Minkyu0424 <yng1404@gmail.com>
2024-02-16 23:07:23 +0900

    :lipstick:[design]: 초기 화면 UI 세팅

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import "./App.css";
-
+import MainPage from "./components/MainPage";
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <MainPage />
   );
 }
 
---
+++ src/components/MainPage.js
@@ -0,0 +1,11 @@
+import "./MainPage.css";
+
+const MainPage = () => {
+  return (
+    <div className=" w-[40%] bg-[#FFC288] h-[100%] m-auto">
+      hi
+    </div>
+  );
+};
+
+export default MainPage;
